87319bc218e2a6adffed53708b107cecbee5f854
Marion Jay M Balugo <j-marion@chronostep.com>
2021-07-31 17:41:07 +0800

    Dockerizing the node application

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const HTTP = require('http');
 const CREATE_SERVER = HTTP.createServer;
 const PORT = process.env.port || 8060;
-const HOSTNAME = '127.0.0.1';
+const HOSTNAME = process.env.HOSTNAME ? '0.0.0.0' : '127.1.0.1';
 
 const server = CREATE_SERVER((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hi World!');
   });
 
 server.listen(PORT, HOSTNAME, () => console.log(`Server running at http://${HOSTNAME}:${PORT}`));
